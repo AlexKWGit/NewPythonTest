4a7b15ef1ace108d2cc297c0b8037e89ca76bf8f
Alexandr Kuznetsov <AlexKWGit@gmail.com>
2024-06-09 20:09:23 +0300

    New v5

--- name_city.py
+++ name_city.py
@@ -1,5 +1,12 @@
-name_user = input("Enter your name: ")
-city_user = input("Enter your city: ")
+class name_city
 
-result = f"Your name is {name_user}. Your city is {city_user}"
-print(result)
+  def __init__(self):
+    pass
+  #Accepts two numbers (x1 and x2), returns the sum of these numbers: x1+x2
+  def add(self, name_user , city_user ):
+    result = "Your name is {name_user}. Your city is {city_user}"
+    return result
+
+# name_user = input("Enter your name: ")
+# city_user = input("Enter your city: ")
+# print(result)
