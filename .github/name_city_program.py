bdf790fca1339680d55199979de4fe76c75bca17
Aleksandr <AlexKWGit@gmail.com>
2024-06-09 18:52:49 +0300

    Create name_city_program.py

---
+++ .github/name_city_program.py
@@ -0,0 +1,8 @@
+def name_city_program():
+    name = input("Enter your name: ")
+    city = input("Enter your city: ")
+    result = f'You name is {name}. You city is {city}'
+    print(result)
+
+if __name__ == "__main__":
+    name_city_program()
