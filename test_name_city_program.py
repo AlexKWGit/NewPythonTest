7b1b390427f7cd8d7618b9478287f7709198d868
Alexandr Kuznetsov <AlexKWGit@gmail.com>
2024-06-09 20:20:52 +0300

    New v7

--- test_name_city_program.py
+++ test_name_city_program.py
@@ -1,10 +1,14 @@
 import unittest
-from unittest.mock import patch
-from name_city import name_user, city_user, result
+from name_city import name_city 
 
 class TestNameCity(unittest.TestCase):
-    @patch('builtins.input', side_effect=['John', 'New York'])
-    def test_name_city(self, mock_input):
-        self.assertEqual(name_user, 'John')
-        self.assertEqual(city_user, 'New York')
-        self.assertEqual(result, "Your name is John. Your city is New York")
+  def setUp(self):
+    self.name_city = name_city()
+  def test_one(self):
+    self.assertEqual(self.name_city.add('John','New York'), 'Your name is John. Your city is New York')
+
+  def test_two(self):
+    self.assertEqual(self.name_city.add('Alex','Minsk'), 'Your name is Alex. Your city is Minsk')
+
+if __name__ == "__main__":
+  unittest.main()
