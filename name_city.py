f1966b5b567f64944b7a501843a74d2ffd98afb3
Alexandr Kuznetsov <AlexKWGit@gmail.com>
2024-06-09 20:32:21 +0300

    New v12

--- name_city.py
+++ name_city.py
@@ -4,7 +4,7 @@ class name_city:
     pass
   #Accepts two numbers (x1 and x2), returns the sum of these numbers: x1+x2
   def add(self, name_user , city_user ):
-    result = 'Your name is ' +{name_user} + '. Your city is ' + {city_user}
+    result = 'Your name is ' + name_user + '. Your city is ' + city_user
     return result
 
 # name_user = input('Enter your name: ')
